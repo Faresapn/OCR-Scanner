36505b4e3e979c70fd4139cc84f97937b239707a
faresa prasetyo nugroho <faresaprasetyon@gmail.com>
2020-03-08 16:19:05 +0700

    push

---
+++ app/src/main/java/com/androdocs/textrecognizer/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.androdocs.textrecognizer
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+
+
+class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_IMAGE_PICK = 2
+    lateinit var imageView: ImageView
+    lateinit var editText: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView)
+        editText = findViewById(R.id.editText)
+    }
+
+
+    fun selectImage(v: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_IMAGE_PICK)
+    }
+    fun CaptureImage(v: View){
+        dispatchTakePictureIntent()
+    }
+    fun next(v: View){
+        startActivity(Intent(this, Main2Activity::class.java))
+    }
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }else if(requestCode == REQUEST_IMAGE_PICK && resultCode == Activity.RESULT_OK) {
+            imageView.setImageURI(data!!.data)
+
+        }
+    }
+
+    fun startRecognizing(v: View) {
+        if (imageView.drawable != null) {
+            editText.setText("")
+            v.isEnabled = false
+            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            val image = FirebaseVisionImage.fromBitmap(bitmap)
+            val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
+            detector.processImage(image)
+                .addOnSuccessListener { firebaseVisionText ->
+                    v.isEnabled = true
+                    processResultText(firebaseVisionText)
+                }
+                .addOnFailureListener {
+                    v.isEnabled = true
+                    editText.setText("Failed")
+                }
+        } else {
+            Toast.makeText(this, "Select an Image First", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    private fun processResultText(resultText: FirebaseVisionText) {
+        if (resultText.textBlocks.size == 0) {
+            editText.setText("No Text Found")
+            return
+        }
+        for (block in resultText.textBlocks) {
+            val blockText = block.text
+            editText.append(blockText + "\n")
+        }
+    }
+}
